995d6ba6b9cb562f2dcfd35969de5f8421054536
amal <amal.t@tabella.cz>
2019-01-09 12:43:28 +0100

    add repository for GSAP Moonfarmer

--- src/js/gsap-scrollmagic-sample.js
+++ src/js/gsap-scrollmagic-sample.js
@@ -10,72 +10,113 @@ import 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min';
 // Execution
 (() => {
     // GreenSock sample
-    const tween = TweenMax.to('#webpack-img', 5, {
-        ease: Power2.easeInOut,
-        repeat: -1,
-        rotation: 360,
-        yoyo: true
-    });
-    // create a restarting animation button	
-	const restartButton = document.getElementById('restart');
-    restartButton.onclick = function(){
-    tween.restart();
-	}
-	 // create a speedUp animation button	
-	const speedUp = document.getElementById('speedUp');
-    speedUp.onclick = function(){
-    tween.timeScale(5);
-	}
-	 // create a origin speed animation button	
-	const originSpeed = document.getElementById('originSpeed');
-    originSpeed.onclick = function(){
-    tween.timeScale(1);
-	}
+    // const tween = TweenMax.to('#webpack-img', 5, {
+    //     ease: Power2.easeInOut,
+    //     repeat: -1,
+    //     rotation: 360,
+    //     yoyo: true
+    // });
 
     // init controller
     const controller = new ScrollMagic.Controller();
 
+// SCENA 1 - Start
     // build scene
     const scene = new ScrollMagic.Scene({
-          duration: 150,
+          duration: 100,
           offset: 50,
           triggerHook: 'onLeave'
     })
     .setTween(
-    	"#webpack-img", 
-    	2, 
+    	"#mission_moon-diffuse", 
+    	4, 
     	{
     		css: {
-    			top: 0, 
-		    	left: 0, 
-		    	x: 0, 
-		    	y: 0,
-		    	border: "1px solid #ff0000",
-		    	maxWidth: "100px",
-		    	height: "auto" 
+    			transform: "rotate(-45deg)"
 		    }
 		})
     // For development only
-    .addIndicators({name: "scene 1 (duration: 150)"})
+    .addIndicators({name: "scene 1 (duration: 100)"})
     .addTo(controller);
+// SCENA 1 - End
 
-        const scene2 = new ScrollMagic.Scene({
-          duration: 50,
-          offset: 100,
-          triggerHook: 'onLeave'
+
+// SCENA 2 - Start
+    const scene2 = new ScrollMagic.Scene({
+      duration: 100,
+      offset: 50,
+      triggerHook: 'onLeave'
     })
     .setTween(
-    	"#webpack-img-2", 
-    	2, 
+    	"#mission_moon-landing", 
+    	3, 
     	{
-    		css: {    			
-		    	y: "50vw",
-		    	border: "1px solid #ff0000",	
-		    	maxWidth: "50px",
-		    	height: "auto"	    	
+    		css: {    		
+		    	transform: "translate(-50%, -100%)"    	
 		    }
 		})
     // For development only
-    .addIndicators({name: "scene 2 (duration: 50)"})
+    .addIndicators({name: "scene 2 (duration: 100)"})
     .addTo(controller);
+// SCENA 2 - End
+
+// SCENA moon - Start
+    const moon = new ScrollMagic.Scene({
+      duration: 100,
+      offset: 1000,
+      triggerHook: 'onLeave'
+    })
+    .setTween(
+        "#mission_moon-diffuse", 
+        3, 
+        {
+            css: {          
+                transform: "rotate(-90deg)"  
+            }
+        })
+    // For development only
+    .addIndicators({name: "moon (duration: 100)"})
+    .addTo(controller);
+// SCENA moon - End
+
+// SCENA 3 - Start
+    const scene3 = new ScrollMagic.Scene({
+      duration: 200,
+      offset: 1000,
+      triggerHook: 'onLeave'
+    })
+    .setTween(
+        "#mission_moon-landing", 
+        3, 
+        {
+            css: {          
+                transform: "translate(-50%, -100%) rotate(-120deg)"     
+            }
+        })
+    // For development only
+    .addIndicators({name: "scene 3 (duration: 100)"})
+    .addTo(controller);
+// SCENA 3 - End
+
+// SCENA 4 - Start
+    const scene4 = new ScrollMagic.Scene({
+      duration: 100,
+      offset: 1000,
+      triggerHook: 'onLeave'
+    })
+    .setTween(
+        "#mission_moon-farm", 
+        3, 
+        {
+            css: {          
+                transform: "translate(-50%, -100%)"     
+            }
+        })
+    // For development only
+    .addIndicators({name: "scene 4 (duration: 100)"})
+    .addTo(controller);
+// SCENA 4 - End
 })();
+
+
+
